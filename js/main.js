2a2f9aafdf24f78559d1c64476b995bb3a2972a8
Ryan James <rcjames1004@gmail.com>
2015-05-14 19:13:04 -0700

    Add images and autogen content
    I was getting sick of updating large chunks of html so I'm autogenning
    it in JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
-$(window).load(function () {
-    // When an about me button is clicked show it's description
-    $(".aboutMe li").click(function (e) {
-        // Hide any currently showing descriptions
-        $(".aboutMeDescriptions div").hide();
+var aboutMeData = [
+    {
+        title: "Education",
+        image: "education.jpg",
+        descriptionHtml: "Education description"
+    },
+    {
+        title: "Research",
+        image: "research.png",
+        descriptionHtml: "Research description"
+    },
+    {
+        title: "Home",
+        image: "home.jpg",
+        descriptionHtml: "Home description"
+    },
+    {
+        title: "Code",
+        image: "code.png",
+        descriptionHtml: "Code description"
+    },
+    {
+        title: "Sports",
+        image: "soccerBall.png",
+        descriptionHtml: "Sports description"
+    },
+    {
+        title: "Gaming",
+        image: "nintendo-controller.jpg",
+        descriptionHtml: "Gaming description"
+    },
+];
 
-        // Show the description for the selected aboutMe button
-        $("." + $(this).attr("data-aboutMeDescriptionClassName")).show();
+$(window).load(function () {
+
+    aboutMeData.forEach(function (aboutMeDataPoint) {
+        var aboutMeButton = $(
+            "<li>" +
+                "<div class='imageContainer'>" +
+                    "<img src='images/" + aboutMeDataPoint.image + "' />" +
+                "</div>" +
+            "</li>");
+        $(".aboutMeButtons").append(aboutMeButton);
+
+        var description = $("<div>" + aboutMeDataPoint.descriptionHtml + "</div>");
+        $(".aboutMeDescriptions").append(description);
+
+        aboutMeButton.click(function () {
+            // Hide any currently showing descriptions
+            $(".aboutMeDescriptions div").hide();
+
+            // Show the description for the selected button
+            description.show();
+        });
     });
 });
