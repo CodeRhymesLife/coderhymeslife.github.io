e08ef9ed3dd04c31af10716e5ce04fa2bbeee15a
Ryan James <rcjames1004@gmail.com>
2015-05-14 23:05:17 -0700

    Small sentence level fixes

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ var aboutMeData = [
                 },
                 {
                     image: "education/michigan-logo.jpg",
-                    text: "Where do I start. I studies Computer Science Engineering at University of Michigan from 2010-2014. " +
+                    text: "Where do I start. I studied Computer Science Engineering at University of Michigan from 2010-2014. " +
                     "While I was there I met my wife, created life long relationships, and learned a ton about myself and the world. I will forever bleed maize and blue. Go blue!",
                 },
                 {
                     image: "education/kc-logo.png",
-                    text: "KC is where it all started. I remember the first day I got my drivers license--the day I put it in my wallet was the same day I got my first speeding ticket (sorry Dad). " +
+                    text: "KC is where it all started. I remember the first day I got my drivers license: the day I put it in my wallet was the same day I got my first speeding ticket (sorry Dad). " +
                         "When I was a freshman I thought fitting in was everything. I soon learned that being myself was overcomming my greatest obstacle. " +
                         "KC is also where I developed my strongest friendships. I don't know where or who I would be without you KC. Good thing I didn't go to " +
                         "<a href='http://loynorrixhighschool.net/'>that school on Kilgore</a> :). Go Giants!",
@@ -38,10 +38,10 @@ var aboutMeData = [
                 {
                     image: "research/semGen.png",
                     text: "\"<a href='http://sbp.bhi.washington.edu/projects/semgen' >SemGen</a> is an experimental software tool for automating the modular composition and decomposition of biosimulation models\" -http://sbp.bhi.washington.edu/projects/semgen" +
-                        "<br/>It's also, in large part, the reason I became interested in grad school. During 2013 I was fortunate enough to volunteer for Max Neal, the creator of SemGen, who I worked with " +
+                        "<br/>It's also, in large part, what brought me to UW. During 2013 I was fortunate enough to volunteer for Max Neal, the creator of SemGen, who I worked with " +
                         "to develop a prototype of the \"stage\"--a drag an drop interface to facilitate biosimulation model composition and decomposition tasks. " +
-                        "During my first couple of quarters at UW, while working with the <a href='http://sbp.bhi.washington.edu/' >Semantics of Biological Processes group</a>, I implemented the first version of the \"stage\" in SemGen. " +
-                        "Check out v1 below! For more information about SemGen and the \"stage\" <a href='http://sbp.bhi.washington.edu/projects/semgen' >click here</a>" +
+                        "A year later, during my first couple of quarters at UW working with the <a href='http://sbp.bhi.washington.edu/' >Semantics of Biological Processes group</a>, I implemented the first version of the \"stage\" in SemGen. " +
+                        "Check out v1 (in test mode) below! For more information about SemGen and the \"stage\" <a href='http://sbp.bhi.washington.edu/projects/semgen' >click here</a>" +
                         "<br/><br/> <iframe width='100%' height='600px' src='semGenStage/stage.html'></iframe>",
                 },
             ],
