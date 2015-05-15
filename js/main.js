f6822cfb2a3daa44b5945cd1b604d7f2c4a1a22d
Ryan James <rcjames1004@gmail.com>
2015-05-14 21:54:46 -0700

    Autogen popup information
    Added more education information

--- js/main.js
+++ js/main.js
@@ -2,32 +2,51 @@ var aboutMeData = [
     {
         title: "Education",
         image: "education.jpg",
-        descriptionHtml: "Education description"
+        description: {
+            contentRows: [
+                {
+                    image: "education/uwbhi.png",
+                    text: "I'm currently a PhD student studying <a href='http://bhi.washington.edu'>Biomedical and Health Informatics</a> @ the University of Washington. Go Huskies!",
+                },
+                {
+                    image: "education/michigan-logo.jpg",
+                    text: "Where do I start. I studies Computer Science Engineering at University of Michigan from 2010-2014. " +
+                    "While I was there I met my wife, created life long relationships, and learned a ton about myself and the world. I will forever bleed maize and blue. Go blue!",
+                },
+                {
+                    image: "education/kc-logo.png",
+                    text: "KC is where it all started. I remember the first day I got my drivers license--the day I put it in my wallet was the same day I got my first speeding ticket (sorry Dad). " +
+                        "When I was a freshman I thought fitting in was everything. I soon learned that being myself was overcomming my greatest obstacle. " +
+                        "KC is also where I developed my strongest friendships. I don't know where or who I would be without you KC. Good thing I didn't go to " +
+                        "<a href='http://loynorrixhighschool.net/'>that school on Kilgore</a> :). Go Giants!",
+                },
+            ]
+        } 
     },
     {
         title: "Research",
         image: "research.png",
-        descriptionHtml: "Research description"
+        description: "Research description"
     },
     {
         title: "Home",
         image: "home.jpg",
-        descriptionHtml: "Home description"
+        description: "Home description"
     },
     {
         title: "Code",
         image: "code.png",
-        descriptionHtml: "Code description"
+        description: "Code description"
     },
     {
         title: "Sports",
         image: "soccerBall.png",
-        descriptionHtml: "Sports description"
+        description: "Sports description"
     },
     {
         title: "Gaming",
         image: "nintendo-controller.jpg",
-        descriptionHtml: "Gaming description"
+        description: "Gaming description"
     },
 ];
 
@@ -55,6 +74,17 @@ $(window).load(function () {
 
         // Add the title and description
         modal.find('.modal-title').text(aboutMeData.title);
-        modal.find('.modal-body').html(aboutMeData.descriptionHtml);
+
+        var contentHtml = "";
+        aboutMeData.description.contentRows.forEach(function (contentRow) {
+            contentHtml +=
+                "<div class='rowOfContent'>" +
+                    "<img src='images/" + contentRow.image + "' />" +
+                    "<p>" + contentRow.text + "</p>" +
+                    "<div class='clear'></div>" +
+                "</div>";
+        });
+
+        modal.find('.modal-body').html(contentHtml);
     });
 });
