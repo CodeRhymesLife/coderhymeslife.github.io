826fbea627ca72345b1b80bf93170fe7f2afa11e
Ryan James <rcjames1004@gmail.com>
2015-09-09 14:59:23 -0700

    Print the resume
    Allow users to print the resume

--- js/main.js
+++ js/main.js
@@ -129,6 +129,31 @@ function getCharge(node) {
 	return getCircleRadius(node) * -100;
 }
 
-function printElement(elementToPrint) {
-	alert("Printing " + elementToPrint);
+function printElement(elem, append, delimiter) {
+    var domClone = elem.cloneNode(true);
+
+    var $printSection = document.getElementById("printSection");
+
+    if (!$printSection) {
+        $printSection = document.createElement("div");
+        $printSection.id = "printSection";
+        document.body.appendChild($printSection);
+    }
+
+    if (append !== true) {
+        $printSection.innerHTML = "";
+    }
+
+    else if (append === true) {
+        if (typeof (delimiter) === "string") {
+            $printSection.innerHTML += delimiter;
+        }
+        else if (typeof (delimiter) === "object") {
+            $printSection.appendChlid(delimiter);
+        }
+    }
+
+    $printSection.appendChild(domClone);
+
+    window.print();
 }
