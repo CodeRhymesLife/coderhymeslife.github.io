b732b4aa1f5b4e718a8d470b05581abfdea50bb6
drryanjames <rcjames1004@gmail.com>
2016-03-04 11:31:40 -0800

    Clean up code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+$(window).load(function () {
+    createGraph();
+    attachModalEvents();
+});
+
 Handlebars.registerHelper('renderResumeSection', function () {
     // Default template
     var templateName = "detailedDescriptionSectionResumeTemplate";
@@ -16,8 +21,8 @@ Handlebars.registerHelper('printArray', function (arr, options) {
 });
 
 var graph;
-$(window).load(function () {
-	var data = [
+function createGraph() {
+    var data = [
         aboutMeData.cv,
         aboutMeData.education,
         aboutMeData.research,
@@ -26,98 +31,61 @@ $(window).load(function () {
         aboutMeData.fun,
     ];
 
-	graph = new Graph();
-	
-	var centerNode = {
-		id: "center",
-		className: "center",
-		radiusOptions: {
-			"small": 60,
-			"large": 100,
-		},
-		r: function () {
-			return getCircleRadius(this);
-		},
-		charge: function () {
-			return getCharge(this);
-		},
-		img: {
-			src: "images/avatar-profile.png",
-		},
-		click: function() {
-				window.location.reload();
-		},
-	}
-	graph.addNode(centerNode);
-	
-	data.forEach(function (item) {
-		var aboutMeNode = {
-			id: item.title,
-			title: item.title,
-			className: "aboutMeNode",
-			radiusOptions: {
-				"small": 40,
-				"large": 60,
-			},
-			r: function () {
-				return getCircleRadius(this);
-			},
-			charge: function () {
-				return getCharge(this);
-			},	
-			click: function() {
-				window.location.href = "#" + item.title;
-			},
-		};
-		graph.addNode(aboutMeNode);
-		
-		// Link the new node to the center node
-		graph.addLink({
-			source: centerNode,
-			target: aboutMeNode,
-		});
-	});
-	graph.update();
-	
-    $('#aboutMeDescriptionmodal').on('show.bs.modal', function (event) {
-        var modal = $(this)
+    graph = new Graph();
 
-        var aboutMeData = modal.data("aboutMeData");
-
-        // Add the title and description
-        modal.find('.modal-title').text(aboutMeData.title);
-
-        // Choose the appropriate template based on what we're rendering
-        var templateName = "rowsOfContentTemplate";
-        if (aboutMeData.title == "CV")
-            templateName = "cvTemplate";
-
-        var source = $("#" + templateName).html();
-        var template = Handlebars.compile(source);
-
-        modal.find('.modal-body')
-            .addClass(aboutMeData.class || "")
-            .html(template(aboutMeData));
-    });
-
-    // Use the hash to navigate between functions
-    var navigate = function () {
-        // Get the content after the hash
-        var hash = location.hash.replace("#", "").toLowerCase();
-
-        // Hide the modal if it's open
-        $('#aboutMeDescriptionmodal').modal('hide');
-
-		if(aboutMeData[hash]) {
-			$('#aboutMeDescriptionmodal').data("aboutMeData", aboutMeData[hash])
-            $('#aboutMeDescriptionmodal').modal('show');
-		}
+    // Add the center node to the graph
+    var centerNode = {
+        id: "center",
+        className: "center",
+        radiusOptions: {
+            "small": 60,
+            "large": 100,
+        },
+        r: function () {
+            return getCircleRadius(this);
+        },
+        charge: function () {
+            return getCharge(this);
+        },
+        img: {
+            src: "images/avatar-profile.png",
+        },
+        click: function () {
+            window.location.reload();
+        },
     }
-    $(window).bind('hashchange', navigate);
+    graph.addNode(centerNode);
 
-    // Navigate to the proper part of the page based on the hash
-    navigate();
-});
+    // Create a graph node for each data element
+    data.forEach(function (item) {
+        var aboutMeNode = {
+            id: item.title,
+            title: item.title,
+            className: "aboutMeNode",
+            radiusOptions: {
+                "small": 40,
+                "large": 60,
+            },
+            r: function () {
+                return getCircleRadius(this);
+            },
+            charge: function () {
+                return getCharge(this);
+            },
+            click: function () {
+                window.location.href = "#" + item.title;
+            },
+        };
+        graph.addNode(aboutMeNode);
+
+        // Link the new node to the center node
+        graph.addLink({
+            source: centerNode,
+            target: aboutMeNode,
+        });
+    });
+    graph.update();
+}
 
 function getCircleRadius(node) {
 	return graph.w <= 480 ?
@@ -156,4 +124,47 @@ function printElement(elem, append, delimiter) {
     $printSection.appendChild(domClone);
 
     window.print();
+}
+
+function attachModalEvents() {
+    // Hook up model events
+    $('#aboutMeDescriptionmodal').on('show.bs.modal', function (event) {
+        var modal = $(this)
+
+        var aboutMeData = modal.data("aboutMeData");
+
+        // Add the title and description
+        modal.find('.modal-title').text(aboutMeData.title);
+
+        // Choose the appropriate template based on what we're rendering
+        var templateName = "rowsOfContentTemplate";
+        if (aboutMeData.title == "CV")
+            templateName = "cvTemplate";
+
+        var source = $("#" + templateName).html();
+        var template = Handlebars.compile(source);
+
+        modal.find('.modal-body')
+            .addClass(aboutMeData.class || "")
+            .html(template(aboutMeData));
+    });
+
+    $(window).bind('hashchange', navigate);
+
+    // Navigate to the proper part of the page based on the hash
+    navigate();
+}
+
+// Use the hash to navigate between functions
+function navigate() {
+    // Get the content after the hash
+    var hash = location.hash.replace("#", "").toLowerCase();
+
+    // Hide the modal if it's open
+    $('#aboutMeDescriptionmodal').modal('hide');
+
+    if (aboutMeData[hash]) {
+        $('#aboutMeDescriptionmodal').data("aboutMeData", aboutMeData[hash])
+        $('#aboutMeDescriptionmodal').modal('show');
+    }
 }
