948b948b83da43946b970e243651d68f03891f31
Ryan James <rcjames1004@gmail.com>
2016-03-15 01:02:20 -0700

    Add source code links to research

--- js/aboutMeData.js
+++ js/aboutMeData.js
@@ -350,7 +350,7 @@ aboutMeData.education = {
     content: [
         {
             image: "education/uwbhi.png",
-            text: "I'm currently a PhD student studying <a href='http://bhi.washington.edu'>Biomedical and Health Informatics</a> @ the University of Washington. Go Huskies!",
+            text: "I'm currently a PhD student studying <a href='http://bhi.washington.edu'  target='_blank'>Biomedical and Health Informatics</a> @ the University of Washington. Go Huskies!",
         },
         {
             image: "education/michigan-logo.jpg",
@@ -362,7 +362,7 @@ aboutMeData.education = {
             text: "KC is where it all started. I remember the first day I got my drivers license: the day I put it in my wallet was the same day I got my first speeding ticket (sorry Dad). " +
                 "When I was a freshman I thought fitting in was everything. I soon learned that being myself was overcomming my greatest obstacle. " +
                 "KC is also where I developed my strongest friendships. I don't know where or who I would be without you KC. Good thing I didn't go to " +
-                "<a href='http://loynorrixhighschool.net/'>that school on Kilgore</a> :). Go Giants!",
+                "<a href='http://loynorrixhighschool.net/'  target='_blank'>that school on Kilgore</a> :). Go Giants!",
         },
     ],
 };
@@ -375,11 +375,11 @@ aboutMeData.research = {
             text: "My latest research explores ways to improve medical education using augmented reality (AR), virtual reality (VR), natural input, and mathematical models of physiology. " +
 				"Learning in AR and VR opens up new dimensions for input, and adds a spatial element to learning that makes things easier to remember. " +
 				"It also adds a level of realism to lessons by placing students inside of the learning material. Here's what I've been working on:<br><br><br>" +
-                "This demo shows CadaVR--a virtual reality platform for medical education. This live demo was created by my team and I for a class project." +
+                "This demo shows CadaVR--a virtual reality platform for medical education. This live demo was created by my team and I for a class project.<br><a href='https://github.com/drryanjames/CadaVR'  target='_blank'>Source Code</a>" +
 				"<div class='aspect-ratio-container'><iframe src='https://www.youtube.com/embed/eYyuEjhD-k8' frameborder='0' allowfullscreen></iframe></div><br><br>" +
-                "This demo explores ways to visualize organs for use in medical education. <a href='VirtualOrganExplorer/Web/Web.html' target='_blank'>Try Here On Desktop</a>." +
+                "This demo explores ways to visualize organs for use in medical education. <a href='VirtualOrganExplorer/Web/Web.html' target='_blank'>Try Here On Desktop</a>.<br><a href='https://github.com/drryanjames/Virtual-Heart'  target='_blank'>Source Code</a>" +
 				"<div class='aspect-ratio-container'><iframe src='https://www.youtube.com/embed/7Wl3me1mkxc' frameborder='0' allowfullscreen></iframe></div><br><br>" +
-                "This demo explores ways to use virtual reality and natural input in medical education." +
+                "This demo explores ways to use virtual reality and natural input in medical education.<br><a href='https://github.com/drryanjames/anatomy-explorer'  target='_blank'>Source Code</a>" +
 				"<div class='aspect-ratio-container'><iframe src='https://www.youtube.com/embed/CiExaT2zaKo' frameborder='0' allowfullscreen></iframe></div>",
         },
 		{
@@ -391,11 +391,11 @@ aboutMeData.research = {
         },
         {
             image: "research/semGen.png",
-            text: "\"<a href='http://sbp.bhi.washington.edu/projects/semgen' >SemGen</a> is an experimental software tool for automating the modular composition and decomposition of biosimulation models\" -http://sbp.bhi.washington.edu/projects/semgen" +
+            text: "\"<a href='http://sbp.bhi.washington.edu/projects/semgen'  target='_blank'>SemGen</a> is an experimental software tool for automating the modular composition and decomposition of biosimulation models\" -http://sbp.bhi.washington.edu/projects/semgen" +
                 "<br/>It's also, in large part, what brought me to UW. During 2013 I was fortunate enough to volunteer for Max Neal, the creator of SemGen, who I worked with " +
                 "to develop a prototype of the \"stage\"--a drag an drop interface to facilitate biosimulation model composition and decomposition tasks. " +
-                "A year later, during my first couple of quarters at UW working with the <a href='http://sbp.bhi.washington.edu/' >Semantics of Biological Processes group</a>, I implemented the first version of the \"stage\" in SemGen. " +
-                "Check out v1 (in test mode) below! For more information about SemGen and the \"stage\" <a href='http://sbp.bhi.washington.edu/projects/semgen' >click here</a>" +
+                "A year later, during my first couple of quarters at UW working with the <a href='http://sbp.bhi.washington.edu/'  target='_blank'>Semantics of Biological Processes group</a>, I implemented the first version of the \"stage\" in SemGen. " +
+                "Check out v1 (in test mode) below! For more information about SemGen and the \"stage\" <a href='http://sbp.bhi.washington.edu/projects/semgen'  target='_blank'>click here</a>" +
                 "<br/><br/> <iframe width='100%' height='600px' src='semGenStage/stage.html'></iframe>",
         },
     ],
